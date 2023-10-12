c2209a188ab69dc896943726179ed3ff43684f14
Vasylii <dedkek56@gmail.com>
2023-10-12 22:17:58 +0600

    Реализовал создание пользователя

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+USERS = []  # лист для объектов типа пользователь
+
 from app import views
+from app import models
--- app/views.py
+++ app/views.py
@@ -1,6 +1,36 @@
-from app import app
+from app import app, USERS, models
+from flask import request, Response
+import json
+from http import HTTPStatus
 
 
 @app.route("/")
 def index():
     return "<h1>Hello world</h1>"
+
+
+@app.route("/users/create")
+def user_create():
+    data = request.get_json()
+    id = len(USERS)
+    first_name = data["first_name"]
+    last_name = data["last_name"]
+    email = data["email"]
+
+    # todo: проверить валидность почты
+
+    user = models.User(id, first_name, last_name, email)
+    USERS.append(user)
+    response = Response(
+        json.dumps({
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "email": email,
+                "total_reactions": user.total_reactions,
+                "posts": user.posts,
+            }),
+            HTTPStatus.OK,
+            mimetype="application/json",
+    )
+    return response
